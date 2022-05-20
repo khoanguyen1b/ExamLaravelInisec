be8cf3341cf9e7a726babefcc5d5f6cfa504ab4c
Khoa Nguyen <khoa.nguyen@dragonflyteam.com>
2022-05-21 02:11:22 +0700

    Update env

--- resources/views/new_post_mail.blade.php
+++ resources/views/new_post_mail.blade.php
@@ -1,7 +1,108 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: khoa
- * Date: 5/21/22
- * Time: 12:44 AM
- */
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="../../css/app.css">
+</head>
+
+<body style="-moz-box-sizing:border-box;-ms-text-size-adjust:100%;-webkit-box-sizing:border-box;-webkit-text-size-adjust:100%;Margin:0;box-sizing:border-box;color:#000!important;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.3;margin:0;min-width:100%;padding:0;text-align:left;width:100%!important">
+<table class="body" data-made-with-foundation style="Margin:0;background:#fefefe;background-color:#eee;border-collapse:collapse;border-spacing:0;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;height:100%;line-height:1.6;margin:0;padding:0;text-align:left;vertical-align:top;width:100%">
+    <tr style="padding:0;text-align:left;vertical-align:top">
+        <td class="float-center" align="center" valign="top" style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0 auto;border-collapse:collapse!important;color:#515b5e;float:none;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;hyphens:auto;line-height:1.6;margin:0 auto;padding:0;text-align:center;vertical-align:top;word-wrap:break-word">
+            <center data-parsed style="min-width:580px;width:100%">
+                <table align="center" class="wrapper header float-center" style="Margin:0 auto;border-collapse:collapse;border-spacing:0;float:none;margin:0 auto;padding:0;text-align:center;vertical-align:top;width:100%">
+                    <tr style="padding:0;text-align:left;vertical-align:top">
+                        <td class="wrapper-inner" style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0;border-collapse:collapse!important;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;hyphens:auto;line-height:1.6;margin:0;padding:0;text-align:left;vertical-align:top;word-wrap:break-word">
+                            <table align="center" class="container header-content" style="Margin:0 auto;background:#fefefe;background-color:#fefefe;border-collapse:collapse;border-spacing:0;margin:0 auto;padding:0;text-align:inherit;vertical-align:top;width:580px">
+                                <tbody>
+                                <tr style="color:#fff;padding:0;text-align:-webkit-center;vertical-align:top">
+                                    <td style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0;border-collapse:collapse!important;color:#fff;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;hyphens:auto;line-height:1.6;margin:0;padding:0;text-align:-webkit-center;vertical-align:top;word-wrap:break-word">
+                                        <table class="spacer" style="border-collapse:collapse;border-spacing:0;padding:0;text-align:left;vertical-align:top;width:100%">
+                                            <tbody>
+                                            <tr style="color:#fff;padding:0;text-align:-webkit-center;vertical-align:top">
+                                                <td height="10px" style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0;border-collapse:collapse!important;color:#fff;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:10px;font-weight:400;hyphens:auto;line-height:10px;margin:0;mso-line-height-rule:exactly;padding:0;text-align:-webkit-center;vertical-align:top;word-wrap:break-word">
+                                                    &#xA0;
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <table class="row" style="border-collapse:collapse;border-spacing:0;display:table;padding:0;position:relative;text-align:left;vertical-align:top;width:100%">
+                                            <tbody>
+                                            <tr style="color:#fff;padding:0;text-align:-webkit-center;vertical-align:top">
+                                                <th class="small-12 large-12 columns first last" valign="middle" style="Margin:0 auto;color:#fff;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0 auto;padding:0;padding-bottom:unset;padding-left:16px;padding-right:16px;text-align:-webkit-center;width:564px;word-break:break-word">
+                                                    <table style="border-collapse:collapse;border-spacing:0;padding:0;text-align:left;vertical-align:top;width:100%">
+                                                        <tr style="color:#fff;padding:0;text-align:-webkit-center;vertical-align:top">
+                                                            <th style="Margin:0;color:#fff;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0;padding:0;text-align:-webkit-center;word-break:break-word">
+                                                            </th>
+                                                            <th class="expander" style="Margin:0;color:#fff;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0;padding:0!important;text-align:-webkit-center;visibility:hidden;width:0;word-break:break-word"></th>
+                                                        </tr>
+                                                    </table>
+                                                </th>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <table class="spacer" style="border-collapse:collapse;border-spacing:0;padding:0;text-align:left;vertical-align:top;width:100%">
+                                            <tbody>
+                                            <tr style="color:#fff;padding:0;text-align:-webkit-center;vertical-align:top">
+                                                <td height="10px" style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0;border-collapse:collapse!important;color:#fff;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:10px;font-weight:400;hyphens:auto;line-height:10px;margin:0;mso-line-height-rule:exactly;padding:0;text-align:-webkit-center;vertical-align:top;word-wrap:break-word">
+                                                    &#xA0;
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <table align="center" class="container float-center" style="Margin:0 auto;background:#fefefe;border-collapse:collapse;border-spacing:0;float:none;margin:0 auto;padding:0;text-align:center;vertical-align:top;width:580px">
+                    <tbody>
+                    <tr style="padding:0;text-align:left;vertical-align:top">
+                        <td style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0;border-collapse:collapse!important;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;hyphens:auto;line-height:1.6;margin:0;padding:0;text-align:left;vertical-align:top;word-wrap:break-word">
+                            <table class="spacer" style="border-collapse:collapse;border-spacing:0;padding:0;text-align:left;vertical-align:top;width:100%">
+                                <tbody>
+                                <tr style="padding:0;text-align:left;vertical-align:top">
+                                    <td height="20px" style="-moz-hyphens:auto;-webkit-hyphens:auto;Margin:0;border-collapse:collapse!important;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:20px;font-weight:400;hyphens:auto;line-height:20px;margin:0;mso-line-height-rule:exactly;padding:0;text-align:left;vertical-align:top;word-wrap:break-word">
+                                        &#xA0;
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <table class="row" style="border-collapse:collapse;border-spacing:0;display:table;padding:0;position:relative;text-align:left;vertical-align:top;width:100%">
+                                <tbody>
+                                <tr style="padding:0;text-align:left;vertical-align:top">
+                                    <th class="small-12 large-12 columns first last" style="Margin:0 auto;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0 auto;padding:0;padding-bottom:unset;padding-left:16px;padding-right:16px;text-align:left;width:564px;word-break:break-word">
+                                        <table style="border-collapse:collapse;border-spacing:0;padding:0;text-align:left;vertical-align:top;width:100%">
+                                            <tr style="padding:0;text-align:left;vertical-align:top">
+                                                <th style="Margin:0;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0;padding:0;text-align:left;word-break:break-word">
+                                                    <p style="Margin:0;Margin-bottom:10px;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0;margin-bottom:10px;padding:0;text-align:left">
+                                                        Hi <strong style="font-weight:500;text-transform:capitalize">
+                                                            {{ $data['full_name_owner'] }}
+                                                        </strong></p>
+                                                    <p style="Margin:0;Margin-bottom:10px;color:#515b5e;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:18px;font-weight:400;line-height:1.6;margin:0;margin-bottom:10px;padding:0;text-align:left">
+                                                        New Post with title {{ $data['title'] }} !
+                                                        <a href="{{ $data['domain_url'] }}" target="_blank" style="Margin:0;color:#193952;font-family:'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Fira Sans','Droid Sans','Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:1.3;margin:0;padding:0;text-align:left;text-decoration:none">
+                                                           This Here </a>
+                                                    </p>
+                                                </th>
+                                            </tr>
+                                        </table>
+                                    </th>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </center>
+        </td>
+    </tr>
+</table>
+</body>
+
+</html>
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscribeController;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/subscribe/website', [\App\Http\Controllers\SubscribeController::class, 'subscribeAWebsite']);
+Route::post('/post/create', [\App\Http\Controllers\PostController::class, 'create']);
